ce348657a875f65a99179167a24779a61fc153ee
Martynas Savickis <martynas@ore.lt>
2014-11-20 11:26:47 +0200

    lith->lithuanian

--- src/main/java/de/l3s/boilerpipe/filters/lithuanian/TerminatingBlocksFinder.java
+++ src/main/java/de/l3s/boilerpipe/filters/lithuanian/TerminatingBlocksFinder.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package de.l3s.boilerpipe.filters.lith;
+package de.l3s.boilerpipe.filters.lithuanian;
 
 import de.l3s.boilerpipe.BoilerpipeFilter;
 import de.l3s.boilerpipe.BoilerpipeProcessingException;
